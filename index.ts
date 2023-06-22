e6a3b46a0020e86b85970014f87ec17b9a6dfa85
Nelson Ortega <ne.beltran18@gmail.com>
2023-06-21 20:47:01 -0500

    typescript express and sequelize setup

---
+++ index.ts
@@ -0,0 +1,8 @@
+import server from "./src/app";
+const { conn } = require("./src/db");
+
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
+    console.log("%s listening at 3001");
+  });
+});
---
+++ src/db.ts
@@ -0,0 +1,16 @@
+require("dotenv").config();
+import { Sequelize, Model } from "sequelize";
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false,
+    native: false,
+  }
+);
+
+module.exports = {
+  ...sequelize.models,
+  conn: sequelize,
+};
