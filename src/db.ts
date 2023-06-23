7aa26a371488e1ec5a89644aed55c365ebd584b8
AlvesMeloMatheus <teteu.melo4@gmail.com>
2023-06-23 16:03:21 -0300

    Tabla Users y UsersType

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,34 @@
 require("dotenv").config();
 import { Sequelize, Model } from "sequelize";
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+import users from "./models/users";
+import usersType from "./models/usersType";
+
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DATABASE } = process.env;
+
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DATABASE}`,
   {
-    logging: false,
-    native: false,
+    dialect: "postgres",
+    //logging: false,
+    //native: false,
+    dialectOptions: {
+      ssl: {
+        mode: "require",
+      },
+    },
   }
 );
+
+users(sequelize)
+usersType(sequelize)
+
+const { Users, UsersType } = sequelize.models;
+
+UsersType.hasMany(Users
+);
+
+Users.belongsTo(UsersType);
 
 module.exports = {
   ...sequelize.models,
