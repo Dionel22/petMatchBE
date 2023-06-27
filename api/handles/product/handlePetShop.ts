4f63c4d09c202ddef495ced3ff79a7ca26afb409
Dionel22 <diondiaz293@gmail.com>
2023-06-27 18:41:03 -0300

    handle-name

--- api/handles/product/handlePetShop.ts
+++ api/handles/product/handlePetShop.ts
@@ -1,14 +1,22 @@
 import {  Request, Response } from "express";
 import { getAllProduct, productDetail, postProduct } from "../../controllers/product/controllerProduct";
 
-// trae todos los productos
+
+// trae todos los productos y busca por nombre
 export const getHandleAllProduct = async (req: Request, res: Response) => {
     try {
+        const { name } = req.query;
         const response = await getAllProduct()
-        res.status(200).json(response)
+        if (name) {
+            let responseName = response.filter((el: any) => el.name.toLocaleLowerCase().includes(name));
+            if (responseName.length === 0) return res.status(200).json({ msg: `No product found with the name ${name}.`});
+            return res.status(200).json(responseName)
+        
+        }
+        return res.status(200).json(response)
     } catch (error) {
         console.log(error)
-        res.status(400).json({msg: error})
+        return res.status(400).json({msg: error})
     }
 }
 
