86c94eb2a4ec6785c6e3993a07af10b5b84fc1fa
Nelson Ortega <ne.beltran18@gmail.com>
2023-06-27 20:08:18 -0500

    created get, getById and post for pets

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import productRouter from "./routerProduct/routerProduct";
 import typeProductRouter from "./routerProduct/routerTypeProduct";
+import petRouter from "./routerPet/petRouter";
 
 const router = Router();
 
-router.use("/product", productRouter)
-router.use("/typeProduct", typeProductRouter)
+router.use("/pets", petRouter);
+router.use("/product", productRouter);
+router.use("/typeProduct", typeProductRouter);
 
 export default router;
