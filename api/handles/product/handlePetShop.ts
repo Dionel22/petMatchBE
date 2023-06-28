563ebf9f0801de711badb5592a0266636db3897e
Dionel22 <105127323+Dionel22@users.noreply.github.com>
2023-06-27 21:27:07 -0300

    Ruta product name (#6)
    * handle-name
    
    * handle-name

--- api/handles/product/handlePetShop.ts
+++ api/handles/product/handlePetShop.ts
@@ -1,14 +1,22 @@
 import {  Request, Response } from "express";
 import { getAllProduct, productDetail, postProduct } from "../../controllers/product/controllerProduct";
 
-// trae todos los productos
+
+// trae todos los productos y busca por nombre
 export const getHandleAllProduct = async (req: Request, res: Response) => {
     try {
+        const { name } = req.query;
         const response = await getAllProduct()
-        res.status(200).json(response)
+        if (name) {
+            let responseName = response.filter((el: any) => el.name.toLocaleLowerCase().includes(name));
+            if (responseName.length === 0) return res.status(200).json({ msg: `No product found with the name ${name}.`});
+            return res.status(200).json(responseName)
+        
+        }
+        return res.status(200).json(response)
     } catch (error) {
         console.log(error)
-        res.status(400).json({msg: error})
+        return res.status(400).json({msg: error})
     }
 }
 
@@ -17,10 +25,10 @@ export const getHandleProductDetail = async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
         const response = await productDetail(id)
-        res.status(200).json(response)
+        return res.status(200).json(response)
     } catch (error) {
         console.log(error)
-        res.status(400).json({msg: error})
+        return res.status(400).json({msg: error})
     }
 }
 
@@ -29,9 +37,9 @@ export const postHandleProduct = async (req: Request, res: Response) => {
     try {
         const { name, imagen, price, available, averageRating, typeId } = req.body;
         const response = await postProduct(name, imagen, price, available, averageRating, typeId)
-        res.status(200).json(response)
+        return res.status(200).json(response)
     } catch (error) {
         console.log(error)
-        res.status(400).json({msg: error})
+        return res.status(400).json({msg: error})
     }
 }
