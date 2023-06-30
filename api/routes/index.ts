69f911b3c61c672250264f959633cdc1e6662417
Magui Martin <Magali Martin>
2023-06-30 19:25:52 -0300

    ruta

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import productRouter from "./routerProduct/routerProduct";
 import typeProductRouter from "./routerProduct/routerTypeProduct"; 
 import OrderRouter from "./routerOrders"
+import registerUser from "./routerRegisterUser";
 
 const router = Router();
 
 router.use("/product", productRouter)
 router.use("/typeProduct", typeProductRouter) 
 router.use("/Order", OrderRouter)
+router.use("/register", registerUser)
 
 export default router;
