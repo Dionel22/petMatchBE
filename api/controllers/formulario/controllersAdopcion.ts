fdaa015c1ace6d83a2cac702350e056cc98e9227
Dionel22 <diondiaz293@gmail.com>
2023-07-09 23:12:13 -0300

    cambie controllers en formulario y users

--- api/controllers/formulario/controllersAdopcion.ts
+++ api/controllers/formulario/controllersAdopcion.ts
@@ -3,26 +3,8 @@ const { Adopcions } = require("../../models/Adopcion")
 
 export const createFormulario = async (body: any) => {
     const id = body.id
-    const { name, lastName, email, address, phone, economicSituation, previousPetExperience, petAllergy, properHome, 
-        dailyPetTime, 
-        over18
-        } = body
-        console.log(email)
-    //const responseID = await buscarUsuario(email)
-    //console.log("<--->",responseID)
-   const createFormulario = await Adopcions.create({ 
-                name, 
-                lastName, 
-                email, 
-                address,
-                phone, 
-                economicSituation, 
-                previousPetExperience,
-                 petAllergy, properHome, 
-                 dailyPetTime, 
-                 over18, 
-                 adopcionId: id
-                })
-            return createFormulario  
+    const { name, lastName, email, address, phone, economicSituation, previousPetExperience, petAllergy, properHome, dailyPetTime, over18 } = body
+    const createFormulario = await Adopcions.create({ name,  lastName,  email,  address, phone,  economicSituation,  previousPetExperience, petAllergy, properHome,  dailyPetTime,  over18,  adopcionId: id })
+    return createFormulario  
 }
 
