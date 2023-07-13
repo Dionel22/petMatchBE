a1cd4720b5252ad9be268ea13a89587bf0ff7ea1
Dionel22 <diondiaz293@gmail.com>
2023-07-12 23:30:55 -0300

    cambio en users

--- api/controllers/users/get/getAllUser.ts
+++ api/controllers/users/get/getAllUser.ts
@@ -1,13 +1,19 @@
 import { Users } from "../../../models/User";
 import { UsersType } from "../../../models/UsersType";
+const { Op } = require('sequelize');
 
 export const getAllUser = async () => { 
     const allUser = await Users.findAll({
         include: {
-            model: UsersType,
-            where: { name: 'usuario' } // Filtras por el tipo "usuario"
+          model: UsersType,
+          where: {
+            [Op.or]: [
+              { name: 'usuario' }, // Filtras por el tipo "usuario"
+              { name: 'organizacion' } // Filtras por el tipo "organizacion"
+            ]
+          }
         }
-    });
+      });
     return allUser;
 }
 //softDelete
